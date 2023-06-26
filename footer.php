3177755ff1476014b8daa1c96d5a77c64b3bbf46
Ipfi2022 <103425163+Ipfi2022@users.noreply.github.com>
2023-06-26 08:56:12 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer>
+  <section class id="contact">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row gy-5  ">
+            <div class="col-lg-4">
+              <h3 class="mb-3"> Magic Barber</h3> 
+              <p>Get in touch with us , in every social Media platform</p>
+              <div class="social-links">
+                   <a href="https://web.facebook.com/people/MagicBarber/100086268563799/"><i class="ri-facebook-fill "></i></a>
+                   <a href="https://www.instagram.com/magic__barbershop/?igshid=YmMyMTA2M2Y%3D"><i class="ri-instagram-fill"></i></a>
+                   <a href="#"><i class="ri-whatsapp-fill"></i></a>
+              </div>             
+            </div>
+            <div class="col-lg-3 offset-lg-1">
+              <h4 class="mb-3">Working Hours</h4>
+              <div>
+                <h6>Monday - friday
+                </h6>
+                <p>07:30Am - 07:30Pm</p>
+              </div>
+              <h6>Saturday - Sunday
+              </h6>
+              <p>12:00Pm - 04:00Pm</p>
+            </div>
+            <div class="col-lg-3">
+              <h4>Contact</h4>
+              <p>
+                <i class="ri-map-2-fill"></i>
+                <span>Calderwood Street 23 shop number 8, Queenstown, Eastern Cape 5320</span>
+              </p>
+              <p>
+                <i class="ri-mail-fill"></i>
+                <span>infomagicbarber@gmail.com</span>
+              </p>
+              <p>
+                <i class="ri-phone-fill"></i>
+                <span>(061) 982 2638</span> 
+                <br>
+                <i class="ri-whatsapp-fill"></i>
+                <span>(061) 982 2638</span> 
+              </p>
+            </div>
+            </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-auto">
+               <p>Copyrights Magic Barbershop. All Rights reserved</p>          
+          </div>
+          <div class="col-auto">
+            <p>Desinged by <a href="#" >Ipfi Tshiswaise</a></p>          
+       </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+</footer>
---
+++ index.php
@@ -0,0 +1,299 @@
+<?php
+session_start();
+error_reporting(0);
+include_once('header.php')
+?>
+
+  <!-- // NAVBAR -->
+
+  <!-- HERO -->
+  <section id="hero">
+    <div class="container">
+      <div class="row">
+        <figure>
+        <img src=" ./img/aston2.jpg" class="aston2" alt="aston2">
+        </figure><div class="col-lg-6">
+          <h1>Aston Hero</h1>
+          <P> best barber In Komani</P>
+          <a href="books.php" class="btn btn-brand">book now</a>
+
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+  </section>
+  <!-- // HERO -->
+
+  <!-- ABOUT -->
+  <section id="about">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5">
+          <img src="./img/service_1.jpg" alt="">
+        </div>
+        <div class="col-lg-4 offset-lg-1">
+          <h6>About us</h6>
+          <h1> About Magic babershop</h1>
+          <p> Magic babershop, the best grooming babershop in Queenstown with 5 active and dedicated
+            babers, we always make sure our customers are well taken care of.</p>
+          <img src="" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- // ABOUT -->
+
+  <!-- SERVICES -->
+  <section id="services">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12 intro">
+          <div class="col-lg-4 col-md-6">
+            <h6>Services</h6>
+            <h1>What we provide?</h1>
+            <p> Various stunning styles you can choose from.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="service">
+            <img src="./img/cut.jpg" alt="">
+            <div class="content">
+                <h3 class="h5"></h3>Hair Cutting</h3>
+                <p>Hair Cut is one of our best services, they are different types of style. </p>
+                <a href="#pricing " class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+            </div> 
+        </div> 
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="service">
+        <img src="./img/service_3.jpg" alt="">
+        <div class="content">
+            <h3 class="h5"></h3>Dreadlocks</h3>
+            <p>we do start, washing ,crotching and stylying as well.</p>
+            <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+        </div> 
+    </div> 
+  </div>  
+  <div class="col-lg-4 col-md-6">
+    <div class="service">
+      <img src="./img/blond_3.jpg" alt="">
+      <div class="content">
+          <h3 class="h5"></h3>Blond Hair</h3>
+          <p>You can choose from different colours,  they are all available.</p>
+          <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+      </div> 
+
+    </div>
+  </div> 
+
+  </section>
+  <!-- // SERVICES -->
+
+
+
+<!-- PRICE -->
+<section id="pricing">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-5">
+        <img src="./img/Price.jpg" alt="">
+      </div>
+      <div class="col-lg-4 offset-lg-1">
+        
+        <h1>  Price List</h1>
+        
+        <h6>Dreadlocks</h6> 
+        <p>  Start- R300 <br> Croshing - R250 <br>Washing - R50 <br>
+          Styling - R150            
+          </p> 
+          <h6> Bleaching</h6>
+          <p>Hair Bleaching - R250<br>
+             </p>
+            <div class="blue/cut" price="300"></div>
+            <h6>Cut</h6>
+            <p> HairCut- R60 </p>
+            <div  class="cut" price="300" ></div>
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
+  <!-- // PRICE -->
+
+  <!-- BLOG -->
+
+  <section id="Blog">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 intro text-center">
+        <h6>Blog</h6>
+        <h1>Latest From Our Blog</h1>
+        <p>There are many variations of Styles , you name them we gat you coverd.</p>       
+      </div>
+    </div>  
+    <div class="row gy-4">
+      <div class="col-lg-4 col-md-6">
+        <article class="blog-post">
+          <img src="img/blond.jpg" alt=""> 
+          <div class="date">
+          <div>
+            <div class="day">22</div>
+            <div class="year"> Jan, 2022 </div>
+          </div>
+          </div>
+          <h3 class="h5"> Hair Blonding</h3>
+          <p>You're only as good as your last haircut.</p>
+          <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+        </article>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <article class="blog-post">
+            <img src="img/blond_2.jpg" alt=""> 
+            <div class="date">
+            <div>
+              <div class="day">12</div>
+              <div class="year"> Feb, 2022 </div>
+            </div>
+            </div>
+            <h3 class="h5"> Hair Blonding</h3>
+            <p>Happiness is a new haircut.</p>
+            <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+          </article>
+          </div>
+           
+        </div>
+         <div>
+          <div class="row gy-4">
+            <div class="col-lg-4 col-md-6">
+              <article class="blog-post">
+                <img src="img/blog-haircut1.jpg" alt=""> 
+                <div class="date">
+                <div>
+                  <div class="day">24</div>
+                  <div class="year"> jan, 2022 </div>
+                </div>
+                </div>
+                <h3 class="h5"> Hair Cut</h3>
+                <p>Never underestimate the power of a good haircut.</p>
+                <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+              </article>
+              </div>
+              <div class="col-lg-4 col-md-3">
+                <article class="blog-post">
+                  <img src="img/blogcut.jpg" alt=""> 
+                  <div class="date">
+                  <div>
+                    <div class="day">17</div>
+                    <div class="year"> Feb, 2022 </div>
+                  </div>
+                  </div>
+                  <h3 class="h5"> Hair Cut</h3>
+                  <p>hair is an accoutrement. Hair is jewelry. It’s an accessory.</p>
+                  <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+                </article>
+                </div>
+                 
+              </div>
+              <div class="row gy-4">
+                <div class="col-lg-4 col-md-4">
+                  <article class="blog-post">
+                    <img src="img/ipfi.jpg" alt=""> 
+                    <div class="date">
+                    <div>
+                      <div class="day">22</div>
+                      <div class="year"> April, 2022 </div>
+                    </div>
+                    </div>
+                    <h3 class="h5"> Locks</h3>
+                    <p>Fresh locks, Happy life.</p>
+                    <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+                  </article>
+                  </div>
+                  <div class="col-lg-3.7 col-md-4">
+                    <article class="blog-post">
+                      <img src="img/dread123.jpg" alt=""> 
+                      <div class="date">
+                      <div>
+                        <div class="day">12</div>
+                        <div class="year"> April, 2022 </div>
+                      </div>
+                      </div>
+                      <h3 class="h5"> Locks</h3>
+                      <p>A woman who cuts her hair is about to change her life.</p>
+                      <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+                    </article>
+                    </div>          
+                  </div> 
+         </div>
+         <div class="row gy-4">
+          <div class="col-lg-4 col-md-4">
+            <article class="blog-post">
+              <img src="img/blog4.jpg" alt=""> 
+              <div class="date">
+              <div>
+                <div class="day">29</div>
+                <div class="year"> Jan, 2022 </div>
+              </div>
+              </div>
+              <h3 class="h5"> Locks</h3>
+              <p> I love having no sense of tomorrow or yesterday.</p>
+              <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+            </article>
+            </div>
+            <div class="col-lg-3.7 col-md-4">
+              <article class="blog-post">
+                <img src="img/cut.jpg" alt=""> 
+                <div class="date">
+                <div>
+                  <div class="day">27</div>
+                  <div class="year"> Feb, 2022 </div>
+                </div>
+                </div>
+                <h3 class="h5"> Locks</h3>
+                <p>I believe that we have free will. I believe we get the chance to make choices in our lives.</p>
+                <a href="#pricing" class="link-more">Know More<i class="ri-arrow-right-line icon"></i></a>
+              </article>
+              </div>   
+            </div>
+
+  <!-- // BLOG -->
+  <!-- COUNTER-->
+  <section id="records">
+      <div class="container text-center">
+      <div class="row">
+        <div class="col-lg-3 col-sm-6 counter">
+          <h1>3500</h1>
+          <p> Costomers Served</p>
+        </div>
+        <div class="col-lg-3 col-sm-6 counter">
+          <h1>5</h1>
+          <p> Active Barbers</p>
+        </div>
+        <div class="col-lg-3 col-sm-6 counter">
+          <h1>1</h1>
+          <p> Available Branch</p>
+        </div>
+      </div>
+      </div>
+<!-- // COUNTER -->
+<!-- FOOTER -->
+<?php
+include_once('footer.php')
+?>
+  <!-- // FOOTER -->
+  </section>
+  
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js"></script>
+  <script src="js/main.js"></script>
+  
+</body>
+</html>
